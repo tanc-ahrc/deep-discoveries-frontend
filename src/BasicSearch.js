67089ed04765b153bc89bb1ec21e97fc4aa7addb
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2021-04-13 17:13:03 +0100

    Add DetailSelector.js
    Derived from Kirill Novikov's avatar component, see comment in
    source.

--- src/BasicSearch.js
+++ src/BasicSearch.js
@@ -2,6 +2,7 @@ import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import { useState } from 'react';
+import DetailSelector from './DetailSelector.js';
 import BasicSearchSidebar from './BasicSearchSidebar.js';
 import BasicSearchResults from './BasicSearchResults.js';
 
@@ -11,6 +12,7 @@ export default function BasicSearch({input}) {
     return (
       <Container style={{paddingTop: '10vh', paddingBottom: '10vh'}} height='90vh'>
         <Typography>Click and draw over the image to highlight areas of interest.</Typography>
+        <DetailSelector src={input.url} shadingColor='black' shadingOpacity={0.5}/>
       </Container>
     )
   }
---
+++ src/DetailSelector.js
@@ -0,0 +1,152 @@
+/* Derived from src/avatar.jsx, in master branch of
+    https://github.com/kirill3333/react-avatar.git, commit
+    60d7a5f728ce276d9cbe84f1773ebf32abddf995.
+ * avatar.jsx copyright (c) 2017 Kirill Novikov, MIT license
+ * All changes copyright (c) 2021 Crown Copyright (The National Archives), MIT license
+ * For MIT license, see https://github.com/tanc-ahrc/deep-discoveries-interface-building-blocks/blob/master/LICENSE.
+ */
+
+import React from 'react'
+import { useState, useEffect } from 'react';
+import Konva from 'konva/lib/Core';
+import 'konva/lib/shapes/Image';
+import 'konva/lib/shapes/Rect';
+import 'konva/lib/shapes/Circle';
+
+export default function DetailSelector({src, shadingColor, shadingOpacity}) {
+  const containerId = 'imageviewer_component';
+  const [image] = useState(new Image());
+  image.src = src;
+  const [width, setWidth] = useState(image.naturalWidth);
+  const [height, setHeight] = useState(image.naturalHeight);
+
+  React.useEffect(() => {
+    // Scale to the smaller dimension
+    let widthScale = width / image.naturalWidth;
+    let heightScale = height / image.naturalHeight;
+    const scale = widthScale < heightScale ? widthScale : heightScale;
+
+    // Base selector size on the smaller factor. Magic constant arrived at by trial and error.
+    const selectorSize = widthScale < heightScale ? image.naturalWidth  * scale * 0.025 * 0.5
+                                                  : image.naturalHeight * scale * 0.025 * 0.5;
+
+    // TODO: Could make more sense to create a subclass of Rect.
+    //       This would handle the boilerplate repetition in the
+    //       event handlers.
+    function initHole(x, y, opacity) {
+      return new Konva.Circle({
+        x: x,
+        y: y,
+        radius: selectorSize * 2,
+        fillPatternImage: image,
+        fillPatternOffset: { x: x, y: y },
+        fillPatternScale: { x: scale, y: scale },
+        opacity: opacity
+      });
+    }
+
+    const stage = new Konva.Stage({
+      container: containerId,
+      width: image.naturalWidth * scale,
+      height: image.naturalHeight * scale
+    });
+
+    const background = new Konva.Image({
+      x: 0,
+      y: 0,
+      width: image.naturalWidth * scale,
+      height: image.naturalHeight * scale,
+      image: image
+    });
+
+    const shading = new Konva.Rect({
+      x: 0,
+      y: 0,
+      width: background.width(),
+      height: background.height(),
+      fill: shadingColor,
+      strokeWidth: 0,
+      opacity: shadingOpacity,
+      visible: true
+    });
+
+    const select = initHole(0, 0, 1);
+    select.visible(false);
+    select.draggable(true);
+    select.stroke('white');
+    select.strokeWidth(1);
+    //TODO: May want to fiddle the size of select so that the part covered
+    //      by the stroke is not included in the selection.
+
+    const layer = new Konva.Layer();
+    layer.add(background);
+    layer.add(shading);
+    layer.add(select);
+    stage.add(layer);
+
+    stage.on("mouseenter", (e) => {
+      select.visible(true);
+      stage.container().style.cursor = 'none';
+      layer.draw();
+    });
+
+    stage.on("mouseleave", (e) => {
+      select.visible(false);
+      stage.container().style.cursor = 'default';
+      layer.draw();
+    });
+
+    stage.on("mousemove", (e) => {
+      const x = e.evt.layerX - selectorSize;
+      const y = e.evt.layerY - selectorSize;
+      select.x(x);
+      select.y(y);
+      select.fillPatternOffset({x: x, y: y});
+      layer.batchDraw();
+    });
+
+    stage.on("mousedown dragmove", (e) => {
+      const x = e.evt.layerX - selectorSize;
+      const y = e.evt.layerY - selectorSize;
+      const hole = initHole(x, y, 1);
+      layer.add(hole);
+      layer.batchDraw();
+    });
+
+    layer.draw();
+  }, [width, height, shadingColor, shadingOpacity, image]);
+
+  /* Resizing tricks following https://www.pluralsight.com/guides/re-render-react-component-on-window-resize */
+  useEffect(
+    () => {
+      const e = document.getElementById(containerId);
+      function handleResize() {
+        // -1 here to avoid infinite loops when the width is fractionally smaller than the offsetWidth
+        if(width < e.offsetWidth - 1 ||      // make the image bigger if it is not using all the space
+          e.offsetWidth < e.scrollWidth) {   // make the image smaller if it is using too much space
+          setWidth(e.offsetWidth);
+        }
+
+        // -1 here to avoid infinite loops when the height is fractionally smaller than the offsetHeight
+        if(height < e.offsetHeight - 1 ||    // make the image bigger if it is not using all the space
+          e.offsetHeight < e.scrollHeight) { // make the image smaller if it is using too much space
+          setHeight(e.offsetHeight);
+        }
+      }
+      handleResize(); //ensure that we are sized correctly
+      window.addEventListener('resize', handleResize);
+
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      }
+    },
+    [image, width, height]
+  );
+
+  /* TODO: The sizing should be handled at the DetailSelector component and passed down. */
+  return (
+    <div width='100%'>
+      <div style={{overflow: 'hidden', height: '70vh', width: '70vw'}} id={containerId} />
+    </div>
+  );
+}
