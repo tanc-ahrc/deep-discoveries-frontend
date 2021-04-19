02c0dcb0d599f17f360a0d36870bde0826428cc2
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2021-04-19 16:53:44 +0100

    Factor ResultImage out of BasicSearchResults
    This will let us use ResultImage in DetailSelection

--- src/BasicSearchResults.js
+++ src/BasicSearchResults.js
@@ -14,8 +14,7 @@ import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
 import AddCircleOutlineOutlinedIcon from '@material-ui/icons/AddCircleOutlineOutlined';
 import RemoveCircleOutlineOutlinedIcon from '@material-ui/icons/RemoveCircleOutlineOutlined';
 import Masonry from 'react-masonry-css';
-import Konva from 'konva/lib/Core';
-import 'konva/lib/shapes/Image';
+import ResultImage from './ResultImage.js';
 
 const useStyles = makeStyles((theme) => ({
   masonry: {
@@ -215,59 +214,4 @@ function ResultTile({result, detailList, setDetailList, tileSize, ...props}) {
       <CardMedia component={ResultImagePart}/>
     </Card>
   );
-}
-
-/* Derived from src/avatar.jsx, in master branch of
-    https://github.com/kirill3333/react-avatar.git, commit
-    60d7a5f728ce276d9cbe84f1773ebf32abddf995.
- * avatar.jsx copyright (c) 2017 Kirill Novikov, MIT license
- * All changes copyright (c) 2021 Crown Copyright (The National Archives), MIT license
- * For MIT license, see https://github.com/tanc-ahrc/deep-discoveries-interface-building-blocks/blob/master/LICENSE.
- */
-function ResultImage({result}) {
-  const containerId = 'ResultTile_' + result.aid;
-  const [width, setWidth] = useState(0);
-  const [image] = useState(new Image());
-  image.src = result.url;
-
-  useEffect(
-    /* Resizing tricks following
-     * www.pluralsight.com/guides/re-render-react-component-on-window-resize
-     */
-    () => {
-      const e = document.getElementById(containerId);
-      function handleResize() {
-        if(image.width !== e.offsetWidth || e.offsetWidth !== e.scrollWidth) {
-          setWidth(e.offsetWidth);
-        }
-      }
-      handleResize(); //ensure that we are sized correctly
-      window.addEventListener('resize', handleResize);
-
-      image.onload = () => {
-        const scale = width / image.width;
-        const stage = new Konva.Stage({
-          container: containerId,
-          width: width,
-          height: image.height * scale,
-        });
-        const background = new Konva.Image({
-          x: 0,
-          y: 0,
-          width: image.width * scale,
-          height: image.height * scale,
-          image: image,
-        });
-        const layer = new Konva.Layer();
-        layer.add(background);
-        stage.add(layer);
-        layer.draw();
-      };
-
-      return () => {
-        window.removeEventListener('resize', handleResize);
-      }
-    }, [containerId, image, width]);
-
-    return (<div id={containerId}/>);
 }
